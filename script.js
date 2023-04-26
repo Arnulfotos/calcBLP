5a6f5f31fa1a02d78818a86539ab539c7dd4c451
Arnulfotos <manuelleon1996@gmail.com>
2023-04-26 02:05:42 -0700

    Se agrego funcionalida de calculo con buena ui en texto

--- script.js
+++ script.js
@@ -1,29 +1,71 @@
 const PRECIO_ENTRADA_GENERAL = 105;
+const PRECIO_CAMPING = 400;
 
 let date = new Date();
+
 let currentDate = `Fecha: ${date.getDate()}/${
   date.getMonth() + 1
 }/${date.getFullYear()}`;
 
 document.getElementById("d1").innerHTML = currentDate;
 
+let currentYear = `&copy; Arnulfotos ${date.getFullYear()}`
+
+document.getElementById("copyright").innerHTML = currentYear;
+
 document.getElementById("form").addEventListener("submit", computeResult);
 
 function computeResult(e) {
-  const UIdias = document.getElementById("entrada-general").value;
-  const UIcamping = document.getElementById("camping").value;
-  const UInumDePersonas = document.getElementById("num-personas").value;
+	const UIdias = document.getElementById("entrada-general").value;
+	const UIcamping = document.getElementById("camping").value;
+	const UInumDePersonas = document.getElementById("num-personas").value;
+	const UInumDeAutos = document.getElementById("auto").value;
 
-  let div = document.getElementById("preview");
+	const calcEntradaGeneral = UInumDePersonas * UIdias * PRECIO_ENTRADA_GENERAL
+	const calcCamping = UInumDeAutos * UIcamping * PRECIO_CAMPING;
 
-  /* ENTRADA GENERAL */
-  div.innerHTML += `<p> El total de por ${UInumDePersonas} persona(s) para ${UIdias} dia(s) es de $${
-    UInumDePersonas * UIdias * PRECIO_ENTRADA_GENERAL
-  } pesos por un horario de 9 a.m. a 6 p.m. </p> <br> <br>`;
+	const calcTotal = calcCamping + calcEntradaGeneral
 
-  /* CAMPING */
+	let div = document.getElementById("preview");
 
-  console.log(UIcamping, UIdias, UInumDePersonas, PRECIO_ENTRADA_GENERAL);
 
-  e.preventDefault();
+
+	let UIvalores = [UIdias, UInumDePersonas, UInumDeAutos, UIcamping]
+
+	let palabrasSingulares = ["dia", "persona", "auto", "noche"]
+	let palabrasSingulares2 = [...palabrasSingulares]
+
+	for (i = 0; i < palabrasSingulares.length; i++) {
+
+		if (UIvalores[i] > 1) {
+			palabrasSingulares[i] += "s"
+		}
+
+	}
+
+
+
+	const datoTextoCalc = {}
+
+	palabrasSingulares2.forEach((element, index) => {
+		datoTextoCalc[element] = palabrasSingulares[index];
+	});
+
+	console.log(datoTextoCalc)
+
+
+
+	/* ENTRADA GENERAL */
+	div.innerHTML += `<p> El total por ${UInumDePersonas} ${datoTextoCalc.persona} para ${UIdias} ${datoTextoCalc.dia} es de $${calcEntradaGeneral} pesos por un horario de 9 a.m. a 6 p.m. </p> <br> <br>`;
+
+	/* CAMPING */
+	div.innerHTML += `<p> El total por ${UInumDeAutos} ${datoTextoCalc.auto} para ${UIcamping} ${datoTextoCalc.noche} es de $${calcCamping} pesos por un horario de 6 p.m. a 9 a.m. </p> <br> <br>`;
+
+	/* TOTAL */
+
+	div.innerHTML += `<p> El total a pagar es de $${calcTotal} pesos</p> <br>  <hr /> <br>`;
+
+
+
+	e.preventDefault();
 }
